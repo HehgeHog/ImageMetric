5821baeb8bd0b96b7076a8338ea2349095e4a9ea
Ivan <95871361+HehgeHog@users.noreply.github.com>
2024-06-11 16:34:06 +0300

    Update Functions.cpp

--- Functions.cpp
+++ Functions.cpp
@@ -1,6 +1,28 @@
-﻿#include"Functions.h"
+#include"Functions.h"
 #include<opencv2/opencv.hpp>
 #include<opencv2/highgui.hpp>
 
+int* Functions::Hist8(cv::Mat img)
+{
+    //определение размеров и создание матрицы гистограммы
+    int cols = img.cols;
+    int rows = img.rows;
+    int temporary = 0;
 
+    static int hist[256]{};
+
+    memset(hist,0,256); // заполнение массива нулями
+
+    //рассчет значений гистограммы
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            temporary = img.at<uchar>(i,j);
+            hist[temporary] += 1;
+        }
+    }
+
+    return hist;
+}
 
