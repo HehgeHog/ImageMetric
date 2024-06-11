7db868c4a17702226935c68d43321a7c3b0442f3
Ivan <95871361+HehgeHog@users.noreply.github.com>
2024-06-11 16:34:37 +0300

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,4 +1,4 @@
-﻿#include"Functions.h"
+#include"Functions.h"
 #include<opencv2/opencv.hpp>
 #include<iostream>
 #include<string>
@@ -6,10 +6,16 @@
 
 int main()
 {
-	cv::Mat img = cv::imread("images/1-A.bmp");
+	cv::Mat img = cv::imread("images/1-A.bmp", 0);
+	int* hist;
+	hist = Functions::Hist8(img);
+
+	for (int i = 0; i < 256; i++)
+	{
+		std::cout << i << ": " << hist[i] << std::endl;
+	}
 
 	cv::imshow("original", img);
-
 	if (cv::waitKey(0) == 27) return 0;
 	return 0;
 }
