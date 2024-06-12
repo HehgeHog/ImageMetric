7b77f69517acfcc0aa00132c559b2978d55cd677
Ivan <95871361+HehgeHog@users.noreply.github.com>
2024-06-12 16:27:46 +0300

    Add files via upload

--- Functions.h
+++ Functions.h
@@ -1,9 +1,12 @@
 #pragma once
 #include<string>
+#include<vector>
+#include<iostream>
 #include<opencv2/opencv.hpp>
 
 class Functions
 {
 public:
-	static int* Hist8(cv::Mat img);
+	// оценочная функция резкости и контраста на основе абсолютного центрального момента
+	static double ACMO(cv::Mat& img); //Absolute Central Moment Operator
 };
--- project.cpp
+++ project.cpp
@@ -1,21 +1,26 @@
-#include"Functions.h"
+﻿#include"Functions.h"
 #include<opencv2/opencv.hpp>
 #include<iostream>
 #include<string>
+#include<vector>
 
 
 int main()
 {
 	cv::Mat img = cv::imread("images/1-A.bmp", 0);
-	int* hist;
-	hist = Functions::Hist8(img);
 
-	for (int i = 0; i < 256; i++)
+	if(img.empty())
 	{
-		std::cout << i << ": " << hist[i] << std::endl;
+		std::cout << "Image reading error" << std::endl;
+		return -1;
 	}
 
 	cv::imshow("original", img);
+	
+	double coffACMO = Functions::ACMO(img);
+	
+	std::cout << "ACMO: " << coffACMO << std::endl;
+
 	if (cv::waitKey(0) == 27) return 0;
 	return 0;
 }
