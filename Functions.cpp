b2ed37118a3a1036bd639e198852546d0c58cf0b
Ivan <95871361+HehgeHog@users.noreply.github.com>
2024-06-12 16:28:32 +0300

    Add files via upload

--- Functions.cpp
+++ Functions.cpp
@@ -1,28 +1,108 @@
-#include"Functions.h"
+﻿#include"Functions.h"
+#include<cmath>
+#include<vector>
+#include<iostream>
 #include<opencv2/opencv.hpp>
 #include<opencv2/highgui.hpp>
+#include<opencv2/imgcodecs.hpp>
 
-int* Functions::Hist8(cv::Mat img)
+static int Sum(std::vector<int>& hist)
 {
-    //определение размеров и создание матрицы гистограммы
-    int cols = img.cols;
-    int rows = img.rows;
+    int sum = 0;
+
+    //подсчет количества всех пикселей
+    for (int i = 0; i < hist.size(); i++)
+    {
+        sum += hist.at(i);
+    }
+
+    return sum;
+}
+static double Probability(std::vector<int>& hist, int sum, int brightness)
+{
+    return (double)hist.at(brightness)/(double)sum;
+}
+static double AverageHist(std::vector<int>& hist)
+{
+    int sum = 0;
+    //сумма всех значений пикселей (сумма яркостей)
+    for (int i = 0; i < hist.size(); i++)
+    {
+        sum += hist.at(i) * i;
+    }
+
+    return sum / pow(hist.size(),2);
+}
+static std::vector<int> Hist(cv::Mat& img, int size_hist)
+{
     int temporary = 0;
 
-    static int hist[256]{};
+    std::vector <int> hist(size_hist);
 
-    memset(hist,0,256); // заполнение массива нулями
+    if (hist.empty())
+    {
+        std::cout << "Error creating histogram" << std::endl;
+        std::vector <int> error = {0};
+        return error;
+    }
 
     //рассчет значений гистограммы
-    for (int i = 0; i < rows; i++)
+    unsigned size = img.cols * img.rows * img.channels();
+    for (unsigned i = 0; i < size; i++)
     {
-        for (int j = 0; j < cols; j++)
-        {
-            temporary = img.at<uchar>(i,j);
-            hist[temporary] += 1;
-        }
+        temporary = img.data[i];
+        hist.at(temporary) += 1;
     }
+
+    //проверка заполненности вектора
+    //for (int i = 0; i < hist.size(); i++)
+    //{
+    //    std::cout << i << ": " << hist.at(i) << std::endl;
+    //}
 
     return hist;
 }
+double Functions::ACMO(cv::Mat& img)
+{
+    int size = 0;
+
+    //определение числа уровней яркости изображения
+    switch (img.depth())
+    {
+    case 0:
+    case 1:
+        size = pow(2,8); // 8-bit
+        break;
+    case 2:
+    case 3:
+        size = pow(2,16);
+        break;
+    case 4:
+    case 5:
+        size = pow(2,32);
+        break;
+    case 6:
+        size = pow(2,64);
+        break;
+    default:
+        std::cout << "ACMO: Image depth error" << std::endl;
+        break;
+    }
+
+    std::vector<int> hist = Hist(img, size); 
+    double average = AverageHist(hist);
+    int sum = Sum(hist);
+
+    double p = 0;
+    double coff = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        p = Probability(hist,sum,i);
+        coff += std::abs(i - average) * p;
+    }
+
+    return coff;
+}
+
 
